15e502b27d7320ece99d350a4268ad236e29cfc9
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-10 22:07:49 +0200

    Update Program.cs

--- 01. Reverse Strings/Program.cs
+++ 01. Reverse Strings/Program.cs
@@ -1,19 +1,30 @@
-﻿
-using System.Text;
+
 
 string input = Console.ReadLine();
 
-
 while (input != "end")
 {
-    StringBuilder sb = new StringBuilder();
-
-    for (int i = input.Length - 1; i >= 0; i--)
-    {
-        sb.Append(input[i]);
-    }
-
-    Console.WriteLine($"{input} = {sb}");
+    char[] revInput = input.Reverse().ToArray();
+    Console.WriteLine($"{input} = {string.Join("", revInput)}");
 
     input = Console.ReadLine();
 }
+
+//using System.Text;
+
+//string input = Console.ReadLine();
+
+
+//while (input != "end")
+//{
+//    StringBuilder sb = new StringBuilder();
+
+//    for (int i = input.Length - 1; i >= 0; i--)
+//    {
+//        sb.Append(input[i]);
+//    }
+
+//    Console.WriteLine($"{input} = {sb}");
+
+//    input = Console.ReadLine();
+//}
