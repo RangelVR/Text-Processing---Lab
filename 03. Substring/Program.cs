57735209ca50d7e3bcab15aec77fa689bcf39902
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-10 22:31:55 +0200

    Add files via upload

---
+++ 03. Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿string input = Console.ReadLine();
+string secondInput = Console.ReadLine();
+
+while (secondInput.Contains(input))
+{
+    secondInput = secondInput.Replace(input, "");
+}
+
+Console.WriteLine(secondInput);
+
+//string input = Console.ReadLine();
+//string secondInput = Console.ReadLine();
+
+//while (secondInput.Contains(input))
+//{
+//    int startIndex = secondInput.IndexOf(input);
+//    secondInput = secondInput.Remove(startIndex, input.Length);
+//}
+
+//Console.WriteLine(secondInput);
