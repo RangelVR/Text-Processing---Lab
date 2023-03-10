86edba8171bcc2af7a1345f9b927839faaacd251
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-10 23:05:32 +0200

    Add files via upload

---
+++ 05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,32 @@
+﻿string input = Console.ReadLine();
+
+Console.WriteLine(string.Join("", input.Where(char.IsDigit)));
+Console.WriteLine(string.Join("", input.Where(char.IsLetter)));
+Console.WriteLine(string.Join("", input.Where(x => !char.IsDigit(x) && !char.IsLetter(x))));
+
+
+//string input = Console.ReadLine();
+
+//string numbers = string.Empty;
+//string letters = string.Empty;
+//string symbols = string.Empty;
+
+//foreach (var ch in input)
+//{
+//	if (char.IsDigit(ch))
+//	{
+//		numbers += ch;
+//	}
+//	else if (char.IsLetter(ch))
+//	{
+//		letters += ch;
+//	}
+//	else
+//	{
+//		symbols += ch;
+//	}
+//}
+
+//Console.WriteLine(numbers);
+//Console.WriteLine(letters);
+//Console.WriteLine(symbols);
