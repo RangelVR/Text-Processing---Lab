3aa6e494d12ee92e351cf8ca24a555d38983f4bf
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-10 22:01:50 +0200

    Add files via upload

---
+++ 01. Reverse Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Text;
+
+string input = Console.ReadLine();
+
+
+while (input != "end")
+{
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = input.Length - 1; i >= 0; i--)
+    {
+        sb.Append(input[i]);
+    }
+
+    Console.WriteLine($"{input} = {sb}");
+
+    input = Console.ReadLine();
+}
