e8fd3770ab00fef08cb2c18a89f64e5fba5a5235
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-10 22:14:13 +0200

    Add files via upload

---
+++ 02. Repeat Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+string[] input = Console.ReadLine().Split();
+
+StringBuilder sb = new StringBuilder();
+
+foreach (var word in input)
+{
+	for (int i = 0; i < word.Length; i++)
+	{
+        sb.Append(word);
+    }
+}
+
+Console.WriteLine(sb);
